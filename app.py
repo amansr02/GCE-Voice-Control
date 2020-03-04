f9a1527d910f6bc7b327656818f1098ae65910cb
Aman Srivastav <amansr@umich.edu>
2020-03-03 19:15:18 -0500

    implementing multiple intents in dialogflow. working on support for new intents and improving structure

--- app.py
+++ app.py
@@ -12,38 +12,31 @@ app = Flask(__name__)
 def webhook():
 
     print("webhook"); sys.stdout.flush()
-
+    
     if request.method == 'POST':
 
         dictionary = dict(request.json)
-        project = dictionary["queryResult"]["parameters"]["project_id"]
-        bucket = "gs://"+project+".appspot.com"
-        instance_template_name = dictionary["queryResult"]["parameters"]["Instance_Template_name"]
-        instance_template_size =  dictionary["queryResult"]["parameters"]["Instance_Template_Size"]["number-integer"]
-        region = dictionary["queryResult"]["parameters"]["Master-Region"]
-        zones = dictionary["queryResult"]["parameters"]["Zone"]
-        instance_group_name = dictionary["queryResult"]["parameters"]["Instance_Group_name"]
-        group_size=  dictionary["queryResult"]["parameters"]["Master-Instance_Group_Count"]["number-integer"]
-        instance_size = dictionary["queryResult"]["parameters"]["Master-Instance_Count"]["number-integer"]
-        instance_name = instance_template_name
-
-        #Create Template
-        #create for loop and change instance_name
-        for number in range(0,(instance_template_size)):
-            template(project,bucket,"asdkbad",instance_template_name+str(number))
+        project_id = dictionary["queryResult"]["parameters"]["project_id"]
+        bucket = "gs://"+project_id+".appspot.com"
+        region = dictionary["queryResult"]["parameters"]["region"]
+        zones = dictionary["queryResult"]["parameters"]["zone"]
+        instance_group_name = dictionary["queryResult"]["parameters"]["instance_group_name"]
+        instance_per_group = dictionary["queryResult"]["parameters"]["instance_per_group"]["number-integer"]
+        instance_group_count = dictionary["queryResult"]["parameters"]["instance_group_count"]
+        instance_name = (project_id.split('-'))[0]
 
         #Create Instance
         #create for loop and change instance_name
         c = 0
-        x = (instance_size*group_size)//len(zones)
-        for number in range(0,int(instance_size*group_size),int(x)):
+        x = (instance_per_group*instance_group_count)//len(zones)
+        for number in range(0,int(instance_per_group*instance_group_count),int(x)):
             for n in range(0,int(x)): 
-                instance(project,bucket,instance_name+str(number+n),region+"-"+zones[c])
+                instance(project_id,bucket,instance_name+str(number+n),region+"-"+zones[c])
             c=c+1
 
         #Create group
-        for number in range(0,group_size):
-            group(project,bucket,region+"-"+zones[number],instance_name,instance_size,instance_size)
+        for number in range(0,instance_group_count):
+            group(project_id,bucket,region+"-"+zones[number],instance_name,instance_per_group,instance_per_group)
 
         json_file="" 
         with open("payload.json") as j:
@@ -66,12 +59,10 @@ def group(project,bucket,zone,instance_name,instance_size,group_size):
             bucket=bucket,
             zone=zone,
             instance_templates = instance_name,
-            instance_templates_size=1,
+            instance_templates_size=instance_size,
             group_name = instance_name+"-group",group_size=group_size)
 
 #Create instance templates. dont pass size. do create a while loop
 #naming convention = example1,example2,example3
-
-
 if __name__ == '__main__':
     app.run(host="127.0.0.1",port="80")
